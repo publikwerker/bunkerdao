3b5d735cdf584521a63888d54d623048e39a7e04
Publikwerker <jason.michael.hoffman@gmail.com>
2022-01-06 10:14:50 -0800

    added FAQs section

--- bunkerdao/src/App.js
+++ bunkerdao/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Roadmap from './components/Roadmap';
+import FAQs from './components/FAQs';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Main />
       <Roadmap />
+      <FAQs />
     </div>
   );
 }
