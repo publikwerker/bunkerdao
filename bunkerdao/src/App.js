63950e9a30a885c51819df1cb4d939c819c6dad2
Publikwerker <jason.michael.hoffman@gmail.com>
2022-01-06 09:45:31 -0800

    added mock Roadmap section

--- bunkerdao/src/App.js
+++ bunkerdao/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Roadmap from './components/Roadmap';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Roadmap />
     </div>
   );
 }
---
+++ bunkerdao/src/components/Roadmap.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './Roadmap.css';
+
+export default class Roadmap extends React.Component {
+
+    render() {
+        return(
+            <section className="roadmap">
+                <h2 className="roadmap-title">Roadmap</h2>
+                <h3 className="roadmap-subtitle">Early 2022</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sagittis purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit. Libero nunc consequat interdum varius sit amet. Consectetur lorem donec massa sapien faucibus. Faucibus ornare suspendisse sed nisi. Consectetur lorem donec massa sapien faucibus et molestie ac. Nam aliquam sem et tortor consequat id porta. Cum sociis natoque penatibus et magnis dis parturient. Sit amet facilisis magna etiam tempor orci eu. Volutpat blandit aliquam etiam erat velit scelerisque in. Varius quam quisque id diam vel quam elementum pulvinar etiam. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Ac odio tempor orci dapibus ultrices.
+                </p>
+
+                <h3 className="roadmap-subtitle">Mid 2022</h3>
+                <p>Tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius. Molestie at elementum eu facilisis sed odio morbi quis. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Viverra nam libero justo laoreet sit. Sagittis purus sit amet volutpat consequat mauris nunc. In nibh mauris cursus mattis molestie a iaculis at erat. Blandit volutpat maecenas volutpat blandit. Pharetra convallis posuere morbi leo urna molestie at elementum. Eu non diam phasellus vestibulum lorem sed risus ultricies. Risus in hendrerit gravida rutrum.
+                </p>
+
+                <h3 className="roadmap-subtitle">Late 2022</h3>
+                <p>Laoreet non curabitur gravida arcu ac tortor dignissim. Fermentum odio eu feugiat pretium. Neque aliquam vestibulum morbi blandit cursus. Nulla porttitor massa id neque. Feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Eu tincidunt tortor aliquam nulla facilisi. Nullam eget felis eget nunc. Lectus urna duis convallis convallis tellus id. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Quisque egestas diam in arcu cursus. Tellus molestie nunc non blandit massa enim nec dui. Velit laoreet id donec ultrices tincidunt arcu non. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Justo donec enim diam vulputate ut.
+                </p>
+
+                <h3 className="roadmap-subtitle">Early 2023</h3>
+                <p>Tellus at urna condimentum mattis pellentesque id nibh. Eu facilisis sed odio morbi quis commodo. Id consectetur purus ut faucibus pulvinar. Dui faucibus in ornare quam viverra orci. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Tempus iaculis urna id volutpat. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Eros donec ac odio tempor orci. Facilisi etiam dignissim diam quis. Lectus nulla at volutpat diam ut venenatis tellus in. Congue mauris rhoncus aenean vel. Auctor eu augue ut lectus arcu bibendum at varius. Aliquet risus feugiat in ante metus dictum. Gravida neque convallis a cras semper auctor neque vitae tempus. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Nulla facilisi etiam dignissim diam quis.
+                </p>
+            </section>
+        )
+    }
+}
