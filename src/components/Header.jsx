662f069e40a1067ee53d0efe29390186eaf8c5ec
Publikwerker <jason.michael.hoffman@gmail.com>
2022-01-08 12:48:49 -0800

    changed display font. replaced lorem ipsum in Header and Roadmap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,21 +19,22 @@ export default class Header extends React.Component {
             <div className="header-row">
               <nav>
                 <a href="/">
-                  <span className='logo'>BunkerDAO</span>
+                  <span className='logo'>STR DAO</span>
                 </a>
                 <Button color={"green"} text={"Connect Wallet"} callback={maskOnClick}/>
               </nav>
             </div>
             <div className="header-row intro">
               <div className="header-col">
-                <h1 className="intro-header">A New Era in Time-Share</h1>
+                <h1 className="intro-header">Member Owned Vacation Rentals</h1>
               </div>
               <div className="header-col">
-                <img className="header-image" src="https://via.placeholder.com/300.png/09f/fff?text=Snappy+Header+Image" alt="A fancy NFT hovering over a bunker"></img>
+                <img className="header-image" src="https://via.placeholder.com/300.png/09f/fff?text=Snappy+Header+Image" alt="A fancy NFT hovering over a landscape"></img>
               </div>
             </div>
             <div className="header-row">
-              <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure</p>
+              <p><strong>Short Term Rental DAO</strong> is reimagining community-managed short-term rental properties. Thousands of members have come together to crowdfund vacation rentals. There are two types of memberships for sale. The Silver Card and the Black Card, both with their own unique perks. 
+              </p>
             </div>
             <div className="header-row left button-sea">
               <Button color={"blue"} text={"Buy on Open Sea"} callback={seaOnClick} />
--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -7,21 +7,21 @@ export default class Roadmap extends React.Component {
         return(
             <section className="roadmap">
                 <h2 className="roadmap-title">Roadmap</h2>
-                <h3 className="roadmap-subtitle">Early 2022</h3>
+                <h3 className="roadmap-subtitle">02/01/2022</h3>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Sagittis purus sit amet volutpat consequat mauris nunc congue. Posuere lorem ipsum dolor sit. Libero nunc consequat interdum varius sit amet. Consectetur lorem donec massa sapien faucibus. Faucibus ornare suspendisse sed nisi. Consectetur lorem donec massa sapien faucibus et molestie ac. Nam aliquam sem et tortor consequat id porta. Cum sociis natoque penatibus et magnis dis parturient. Sit amet facilisis magna etiam tempor orci eu. Volutpat blandit aliquam etiam erat velit scelerisque in. Varius quam quisque id diam vel quam elementum pulvinar etiam. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin. Ac odio tempor orci dapibus ultrices.
+                We will drop the VacationDAO Membership NFTs. These NFTs will allow for community access, governance, a wide variety of perks, and games in the near term. 
                 </p>
 
-                <h3 className="roadmap-subtitle">Mid 2022</h3>
-                <p>Tincidunt lobortis feugiat vivamus at augue eget arcu dictum varius. Molestie at elementum eu facilisis sed odio morbi quis. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Viverra nam libero justo laoreet sit. Sagittis purus sit amet volutpat consequat mauris nunc. In nibh mauris cursus mattis molestie a iaculis at erat. Blandit volutpat maecenas volutpat blandit. Pharetra convallis posuere morbi leo urna molestie at elementum. Eu non diam phasellus vestibulum lorem sed risus ultricies. Risus in hendrerit gravida rutrum.
+                <h3 className="roadmap-subtitle">03/01/2022</h3>
+                <p>In early 2022, the VacationDAO community will organize into a DAO tasked with identifying the first properties to acquire from a list provided by experts in Rental Arbitrage Acquisitions.
                 </p>
 
-                <h3 className="roadmap-subtitle">Late 2022</h3>
-                <p>Laoreet non curabitur gravida arcu ac tortor dignissim. Fermentum odio eu feugiat pretium. Neque aliquam vestibulum morbi blandit cursus. Nulla porttitor massa id neque. Feugiat nibh sed pulvinar proin gravida hendrerit lectus a. Eu tincidunt tortor aliquam nulla facilisi. Nullam eget felis eget nunc. Lectus urna duis convallis convallis tellus id. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Quisque egestas diam in arcu cursus. Tellus molestie nunc non blandit massa enim nec dui. Velit laoreet id donec ultrices tincidunt arcu non. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Justo donec enim diam vulputate ut.
+                <h3 className="roadmap-subtitle">Q2 - 2022</h3>
+                <p>The VacationDAO community will support the acquisition of 25-50+ properties/month VacationDAO Website goes live for members to start using their Membership Cards Suggestions for properties to acquire? Join discord and let us know.
                 </p>
 
-                <h3 className="roadmap-subtitle">Early 2023</h3>
-                <p>Tellus at urna condimentum mattis pellentesque id nibh. Eu facilisis sed odio morbi quis commodo. Id consectetur purus ut faucibus pulvinar. Dui faucibus in ornare quam viverra orci. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Tempus iaculis urna id volutpat. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget. Eros donec ac odio tempor orci. Facilisi etiam dignissim diam quis. Lectus nulla at volutpat diam ut venenatis tellus in. Congue mauris rhoncus aenean vel. Auctor eu augue ut lectus arcu bibendum at varius. Aliquet risus feugiat in ante metus dictum. Gravida neque convallis a cras semper auctor neque vitae tempus. Vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt. Nulla facilisi etiam dignissim diam quis.
+                <h3 className="roadmap-subtitle">Q3 - 2022 and Beyond</h3>
+                <p>The VacationDAO community will continue to expand its locations and begin to create a global Vacation Rental membership that is owned and operated by the people. 
                 </p>
             </section>
         )
