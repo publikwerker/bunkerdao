f775750c9c2dbd4b61dd83dc88a979bd3a2d1967
Publikwerker <jason.michael.hoffman@gmail.com>
2022-01-08 13:31:13 -0800

    added copy and images to Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,17 +4,24 @@ import './Card.css';
 export default class Card extends React.Component {
 
     render() {
+        console.log(this.props);
+        console.log(this.props.data);
+
+        const lister = [];
+        this.props.data.list.map(element => {
+            lister.push(<li>{element}</li>);
+        });
+        console.log("lister", lister);
         return (
             <div className='card'>
-            <a href="https://placeholder.com"><img src="https://via.placeholder.com/150"></img></a>
-            <h2 className='card-title'></h2>
+            <a ><img src={this.props.src}></img></a>
+            <h2 className='card-title'>{this.props.data.title}</h2>
+            <p>{this.props.data.total}</p>
+            <p>{this.props.data.text}</p>
             <ul className='card-list'>
-                <li className='card-item'></li>
-                <li className='card-item'></li>
-                <li className='card-item'></li>
-                <li className='card-item'></li>
-                <li className='card-item'></li>
+                {lister}
             </ul>
+            <h3>{this.props.data.price}</h3>
         </div>
         )
     }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import Card from './Card';
 import './Main.css';
+const silver = require('./images/vDAOSilver.png');
+const props1 = {
+    title: "Silver Card Membership NFT",
+    total: "6.363 total",
+    text: "Enjoy the perks of VacationDAO including...",
+    list: [
+        "3 days of free stay in one of our vacation rentals",
+        "Governance rights via $Vacation token, launching in early 2022",
+        "Access and discounts on packages, leisure products, merch, and more",
+        "Access to members-only Discord channels, including guest appearances by business, and celebrity luminaries",
+        "More to come!"
+    ],
+    price: "0.18 ETH"
+}
+const props2 = {
+    title: "Black Card Membership NFT",
+    total: "2,727 total",
+    text: "All the perks of a Silver Card Membership plus...",
+    list: [
+        "4x the governance rights via $Vacation token, launching in early 2022",
+        "4 additional days of stay at one of our Vacation Rental",
+        "% of profit-sharing based on ownership",
+        "Access to rental reciprocity with other Partners who opt-in to participating",
+        "More to come!"
+    ],
+    price: "0.72 ETH"
+}
+const black = require('./images/vDAOBlack.png');
 
 
 export default class Main extends React.Component {
@@ -11,8 +39,8 @@ export default class Main extends React.Component {
         return (
             <main>
                 <div className='card-container'>
-                    <Card />
-                    <Card />        
+                    <Card src={silver} data={props1} />
+                    <Card src={black} data={props2}/>        
                 </div>
             </main>
         )
